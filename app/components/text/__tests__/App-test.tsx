3f61f7247c1e24b0ec7e20d857542deb54f75c99
Irmak Coşar Şahna <irmakcosarsahna@gmail.com>
2021-09-15 12:29:52 +0300

    some bug fixed

---
+++ app/components/text/__tests__/App-test.tsx
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import { Text } from '../index';
+
+it('renders Text', () => {
+  renderer.create(<Text />);
+});
